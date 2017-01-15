cc768cc7488045d65d1ee19cf8129c1d9a0a0b0c
Muhammad Hasan Khan <slashdot10@gmail.com>
2017-01-15 11:41:44 -0800

    move tests to application/tests

---
+++ application/tests/UserIdentityTest.php
@@ -0,0 +1,30 @@
+<?php
+require(__DIR__.'/../../vendor/autoload.php');
+require_once(__DIR__.'/../../vendor/phpunit/phpunit/src/Framework/TestCase.php');
+require_once(__DIR__.'/../components/UserIdentity.php');
+
+class UserIdentityTest extends PHPUnit_Framework_TestCase
+{
+    public function testAuthenticateSucceeds()
+    {
+        $this->execAuthenticate('demo', 'demo', true, UserIdentity::ERROR_NONE);
+    }
+
+    public function testAuthenticateFailsIfUserDoesNotMatch()
+    {
+        $this->execAuthenticate('blah', 'demo', false, UserIdentity::ERROR_USERNAME_INVALID);
+    }
+
+    public function testAuthenticateFailsIfPasswordDoesNotMatch()
+    {
+        $this->execAuthenticate('demo', 'blah', false, UserIdentity::ERROR_PASSWORD_INVALID);
+    }
+
+    private function execAuthenticate($user, $pass, $result, $errorCode) 
+    {
+        $userIdentity = new UserIdentity($user, $pass);
+        $this->assertEquals($userIdentity->authenticate(), $result);
+        $this->assertEquals($userIdentity->errorCode, $errorCode);
+    }
+}
+?>
--- application/tests/bootstrap.php
+++ application/tests/bootstrap.php
@@ -1,10 +1,7 @@
 <?php
+    ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
 
-// change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../../framework/yiit.php';
-$config=dirname(__FILE__).'/../config/test.php';
-
-require_once($yiit);
-require_once(dirname(__FILE__).'/WebTestCase.php');
-
-Yii::createWebApplication($config);
+    require_once(__DIR__.'/../../vendor/yiisoft/yii/framework/yii.php');
+?>
