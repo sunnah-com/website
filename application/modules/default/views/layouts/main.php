e64da773431062524b3db0829c8e93c27787f5c1
Warp Lord <fuddinsiddiqui@gmail.com>
2020-06-07 06:51:32 +0500

    Load same jQuery files on home and main templates

--- application/modules/default/views/layouts/main.php
+++ application/modules/default/views/layouts/main.php
@@ -29,8 +29,8 @@
   <link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
   <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
   <!--<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>-->
-  <script src="/js/jquery.min.js"></script>
-  <script src="/js/jquery-ui.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"></script>
   <script src="/js/jquery.cookie.js"></script>
   <!--<script type="text/javascript" src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js" async></script>-->
 
