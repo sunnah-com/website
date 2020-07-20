77c324f329017b77c62d07b49f4891a899080fb6
Warp Lord <fuddinsiddiqui@gmail.com>
2020-07-20 11:14:35 +0500

    Fix carousel navigation alignment
    Make carousel navigation more opaque

--- application/modules/front/views/index/carousel.php
+++ application/modules/front/views/index/carousel.php
@@ -5,8 +5,8 @@
 <div class="hcarousel_container">
 	<ul id="hcarousel">
 	</ul>
-	<span class="carousel_nav back"><</span>
-	<span class="carousel_nav next">></span>
+	<span class="carousel_nav back">⯇</span>
+	<span class="carousel_nav next">⯈</span>
 </div>
 
 <div align=center style="font-size: 14px; margin-top: 5px;">
