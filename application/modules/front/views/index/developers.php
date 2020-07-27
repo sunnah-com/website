45d614ef66f5e8d19a691d089e3f40fbd12e6248
Glaisher <glaisher.wiki@gmail.com>
2020-07-27 22:33:16 +0500

    Add content to Developers page and add link in footer

--- application/modules/front/views/index/developers.php
+++ application/modules/front/views/index/developers.php
@@ -1,10 +1,39 @@
 
 <div class="about">
 
+<br>
+<div class=section>Developers</div><br>
+
+<div class=content>
 At <a href="/">sunnah.com</a> we are <a href="/about">committed to providing an
 open platform for hadith data that includes data and tools</a>. To this end, we
-have an <a href="#api">API</a> for consumption and 
-<a href="https://github.com/sunnah-com">several open-source projects</a> on 
-which we invite contributions.
+have an API for consumption and <a href="https://github.com/sunnah-com">several open-source projects</a>
+on which we invite contributions.
+</div><br>
+
+<div class=section>API</div>
+<div class=content>
+Our API offers access to verified data of collections of hadith of Prophet Muhammad (&#xFDFA;).
+To check out the API documentation, visit <a href="https://sunnah.api-docs.io">sunnah.api-docs.io</a>.
+You will need an API key to access this data, which you may request by
+<a href="https://github.com/sunnah-com/api/issues/new?template=request-for-api-access.md&title=Request+for+API+access%3A+%5BYour+Name%5D">creating an issue</a>
+on our GitHub repo. You may request for an offline dump of hadith data if that
+is more suitable for your needs.
+</div><br>
+
+<div class=section>Contribute</div>
+<div class=content>
+We are excited that you are interested in contributing to the sunnah.com project.
+Our code repositories are hosted on <a href="https://github.com/sunnah-com">GitHub</a>.
+These are:
+<ul>
+<li><a href="https://github.com/sunnah-com/website">Sunnah.com website</a> - PHP</li>
+<li><a href="https://github.com/sunnah-com/api">API</a> - Python</li>
+<li><a href="https://github.com/sunnah-com/corrections">Corrections</a> - Python and JavaScript</li>
+</ul>
+
+Should you have any questions or want to contact maintainers, just create an issue on GitHub or
+<a href="/contact">send us a message</a>! We will get back to you as soon as we can, insha Allah.
+</div><br>
 
 </div>
