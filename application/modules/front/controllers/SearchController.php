a656f301a06960e095cddc6cbfde669c3a223146
Glaisher <glaisher.wiki@gmail.com>
2020-08-04 08:58:40 +0500

    SearchController: Minor code cleanup

--- application/modules/front/controllers/SearchController.php
+++ application/modules/front/controllers/SearchController.php
@@ -6,14 +6,13 @@ use app\controllers\SController;
 use app\modules\front\models\Search;
 use yii\db\Query;
 use yii\data\Pagination;
+use yii\helpers;
 use yii\widgets\LinkPager;
 use Yii;
 
 class SearchController extends SController
 {
-    protected $_numFound;
     protected $_collections;
-    protected $_spellcheck;
     protected $_searchQuery;
     protected $_highlighted;
     protected $_eurns;
@@ -49,88 +48,71 @@ class SearchController extends SController
     public function actionSearch()
     {
         $query = Yii::$app->request->get('q');
-        if ($query === null) {
+        $query = trim($query);
+        if ($query === '') {
             return Yii::$app->runAction('front/index/index');
         }
 
+        $this->view->params['_searchQuery'] = $query;
+        $this->view->params['_pageType'] = 'search';
+
         $page = Yii::$app->request->get('page', 1);
+        $page = intval($page);
         return $this->processSearch($query, $page);
     }
 
     public function processSearch($query, $page)
     {
-        $this->_viewVars = new \StdClass();
-        $this->view->params['_searchQuery'] = $query;
-        $this->view->params['_pageType'] = "search";
-        $this->pathCrumbs('Search Results - '.htmlspecialchars($query).' (page '.$page.')', '');
-        if (strlen($query) < 1) {
-            return null;
-        }
+        $this->pathCrumbs('Search Results - '.helpers\Html::encode($query).' (page '.$page.')', '');
 
         $searchObject = new Search();
         $results_arr = $searchObject->searchEnglishHighlighted($query, $page);
         if (count($results_arr) == 0) {
-            $errorMsg = "The search engine is currently down. The web administrators have been notified and will be working to get it back up as soon as possible, inshaAllah.";
-            return $this->render('index', ['errorMsg' => $errorMsg]);
+            return $this->searchEngineDown();
         }
-        $eurns = $results_arr[0];
-        $aurns = $results_arr[1];
-        $highlighted = $results_arr[2];
-        $numFound = $results_arr[3];
-        $spellcheck = $results_arr[4];
-        $language = "english";
-        $english = true;
 
-        //Yii::log("query: $query, numFound: $numFound, aurns: ".print_r($aurns), 'info', 'system.web.CController');
+        $language = 'english';
+        [$eurns, $aurns, $highlighted, $numFound, $spellcheck] = $results_arr;
 
-        if ($eurns == null) {
+        if ($eurns === null) {
+            // If no English results were found, do Arabic search
             $results_arr = $searchObject->searchArabicHighlighted($query, $page);
             if (count($results_arr) == 0) {
-                $errorMsg = "The search engine is currently down. The web administrators have been notified and will be working to get it back up as soon as possible, inshaAllah.";
-                return $this->render('index', ['errorMsg' => $errorMsg]);
+                return $this->searchEngineDown();
             }
-            $eurns = $results_arr[0];
-            $aurns = $results_arr[1];
-            $highlighted = $results_arr[2];
-            $numFound = $results_arr[3];
-            $language = "arabic";
-            $english = false;
+
+            $language = 'arabic';
+            [$eurns, $aurns, $highlighted, $numFound, $spellcheck] = $results_arr;
         }
 
         $searchObject->logQuery(addslashes($query), $numFound);
 
         if (is_null($eurns) && is_null($aurns)) {
             // Zero search results
-            $this->_numFound = 0;
-            $this->_spellcheck = $spellcheck;
-            $this->_viewVars->pageType = "search";
-            $this->view->params['_pageType'] = "search";
-            return $this->render('index', [
-                                     'numFound' => 0,
-                                     'spellcheck' => $spellcheck,
-            ]);
+            return $this->render(
+                'index',
+                ['numFound' => 0, 'spellcheck' => $spellcheck]
+            );
         }
 
-        for ($i = 0; $i < count($eurns); $i++) {
-            $eurns[$i] = intval($eurns[$i]);
-        }
+        $eurns = array_map('intval', $eurns);
         $query = new Query();
-        $query = $query->select('*')
-                       ->from('EnglishHadithTable')
-                       ->where(array('in', 'englishURN', $eurns));
+        $query = $query
+            ->select('*')
+            ->from('EnglishHadithTable')
+            ->where(array('in', 'englishURN', $eurns));
         $englishSet = $query->all();
         $english_hadith = array();
         foreach ($englishSet as $row) {
             $english_hadith[array_search($row['englishURN'], $eurns)] = $row;
         }
 
-        for ($i = 0; $i < count($aurns); $i++) {
-            $aurns[$i] = intval($aurns[$i]);
-        }
+        $aurns = array_map('intval', $aurns);
         $query = new Query();
-        $query = $query->select('*')
-                ->from('ArabicHadithTable')
-                ->where(array('in', 'arabicURN', $aurns));
+        $query = $query
+            ->select('*')
+            ->from('ArabicHadithTable')
+            ->where(array('in', 'arabicURN', $aurns));
         $arabicSet = $query->all();
         $arabic_hadith = array();
         foreach ($arabicSet as $row) {
@@ -139,7 +121,7 @@ class SearchController extends SController
 
         // For the Arabic ahadith that have no English match, we need to populate
         // the englishBookName and englishBookID field for display purposes
-        if (!$english && !is_null($eurns)) {
+        if ($language === 'arabic' && !is_null($eurns)) {
             $missing_keys = array_keys($eurns, 0);
             foreach ($missing_keys as $missing_key) {
                 // If for some reason the Arabic hadith doesn't exist (e.g. if the search index is stale)
@@ -162,7 +144,7 @@ class SearchController extends SController
         $viewVars['highlighted'] = $highlighted;
         $viewVars['eurns'] = $eurns;
         $viewVars['aurns'] = $aurns;
-        $viewVars['pairs'] = $searchObject->php_zip($eurns, $aurns);
+        $viewVars['pairs'] = array_map(null, $eurns, $aurns);
         $viewVars['english_hadith'] = $english_hadith;
         $viewVars['arabic_hadith'] = $arabic_hadith;
         $viewVars['numFound'] = $numFound;
@@ -172,14 +154,19 @@ class SearchController extends SController
         $viewVars['resultsPerPage'] = Yii::$app->params['pageSize'];
         $viewVars['collections'] = $this->util->getCollectionsInfo("indexed");
         $this->_pages = new Pagination([
-                               'totalCount' => $numFound,
-                               'pageSize' => Yii::$app->params['pageSize'],
-                               'defaultPageSize' => Yii::$app->params['pageSize'],
-                            ]);
-        $this->_pages->pageSize = Yii::$app->params['pageSize'];
+            'totalCount' => $numFound,
+            'pageSize' => Yii::$app->params['pageSize'],
+            'defaultPageSize' => Yii::$app->params['pageSize'],
+        ]);
         $viewVars['pages'] = $this->_pages;
 
         return $this->render('index', $viewVars);
+    }
+
+    protected function searchEngineDown()
+    {
+        $errorMsg = 'The search engine is currently down. The web administrators have been notified and will be working to get it back up as soon as possible, inshaAllah.';
+        return $this->render('index', ['errorMsg' => $errorMsg]);
     }
 
     public function url_decode($link)
