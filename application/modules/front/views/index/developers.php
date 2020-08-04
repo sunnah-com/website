3282bc60141204ae304ac8377e96c8fa80bc4ab5
ahadith <sunnah@iman.net>
2020-08-04 14:21:34 -0700

    More minor tweaks to the developers page

--- application/modules/front/views/index/developers.php
+++ application/modules/front/views/index/developers.php
@@ -6,7 +6,7 @@
 
 <div class=content>
 At <a href="/">sunnah.com</a> we are committed to providing an
-<a href="/about">open platform for hadith data</a> that includes data and tools. To this end, we
+<a href="/about">open platform for hadith</a> that includes data and tools. To this end, we
 have an API for consumption and <a href="https://github.com/sunnah-com">several open-source projects</a>
 on which we invite contributions.
 </div><br>
@@ -14,25 +14,24 @@ on which we invite contributions.
 <div class=section>API</div>
 <div class=content>
 Our API offers access to a portion of our data comprising the sunnah of the Prophet Muhammad (&#xFDFA;). We will add to the data available as we complete manual checks of the data.
-For API documentation, visit <a href="https://sunnah.api-docs.io">sunnah.api-docs.io</a>.
-You will need an API key to access this data, which you may request by
-<a href="https://github.com/sunnah-com/api/issues/new?template=request-for-api-access.md&title=Request+for+API+access%3A+%5BYour+Name%5D">creating an issue</a>
-on our GitHub repo. You may request an offline dump of hadith data if that
-is more suited to your needs (in progress).
+API documentation is available at <a href="https://sunnah.api-docs.io">sunnah.api-docs.io</a>.
+You will need an API key to access this data; you can
+<a href="https://github.com/sunnah-com/api/issues/new?template=request-for-api-access.md&title=Request+for+API+access%3A+%5BYour+Name%5D">create an issue</a>
+on our GitHub repo to request one. You may also request an offline dump of hadith data if that
+is more suited to your needs (<i>not available yet</i>).
 </div><br>
 
 <div class=section>Contribute</div>
 <div class=content>
-We are excited that you are interested in contributing to the <a href="/">sunnah.com</a> project.
-Our code repositories are hosted on <a href="https://github.com/sunnah-com">GitHub</a>.
-These are:
+We are excited that you are interested in contributing to the <a href="/">sunnah.com</a> project!
+Our code repositories are hosted on <a href="https://github.com/sunnah-com">GitHub</a>:
 <ul>
 <li><a href="https://github.com/sunnah-com/website">The Sunnah.com website</a> - PHP</li>
 <li><a href="https://github.com/sunnah-com/api">Sunnah.com API</a> - Python (Flask)</li>
 <li><a href="https://github.com/sunnah-com/corrections">Corrections</a> - Python and JavaScript</li>
 </ul>
 
-Should you have any questions or want to contact maintainers, just create an issue on GitHub or
+If you have any questions or want to contact the maintainers, create an issue on GitHub or
 <a href="/contact">send us a message</a>! We will get back to you as soon as we can, in sha Allah.
 </div><br>
 
