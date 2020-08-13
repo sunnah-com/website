bd57daeb4cb762a1dec2ca490e03e08314d40bbb
Warp Lord <fuddinsiddiqui@gmail.com>
2020-08-13 19:31:03 +0500

    Added Prev/Next navigation on single Hadith view

--- application/modules/front/views/collection/dispbook.php
+++ application/modules/front/views/collection/dispbook.php
@@ -114,7 +114,7 @@ else {
     <?php } ?>
 
     <a name="0"></a>
-	<div class=AllHadith>
+	<div class="AllHadith<?php echo ($totalCount === 1) ? " single_hadith" : "" ?>">
 	<?php
 
                     // Special case for the rare 0-hadith book with chapters 
@@ -271,6 +271,31 @@ else {
 							}
 						}
 					}
+
+					/**
+					 * Add Hadith Navigation on Single Hadith page
+					 * */
+					if ( $totalCount === 1 && ( isset($nextPermalink) || isset($previousPermalink) )) {
+
+						echo "<div class=\"hadith_nav_wrapper\">";
+
+						if ( isset($previousPermalink) ) {
+							echo "<a class=\"hadith_nav button prev\" title=\"$collection->englishTitle $previousHadithNumber\" href=\"$previousPermalink\">$previousHadithNumber</a>";
+						} else {
+							// Is first in the collection
+							echo "<a class=\"hadith_nav button prev\" disabled=disabled></a>";
+						}
+
+						if ( isset ($nextPermalink) ) {
+							echo "<a class=\"hadith_nav button next\" title=\"$collection->englishTitle $nextHadithNumber\" href=\"$nextPermalink\">$nextHadithNumber</a>";
+						} else {
+							// Is last in the collection
+							echo "<a class=\"hadith_nav button next\" disabled=disabled></a>";
+						}
+						
+						echo "</div>";
+					}
+
 					echo "<!-- <div align=right><i>Content on this page was last updated on ".$this->params['lastUpdated']."</i></div> -->";
 					echo "</div>";
 
