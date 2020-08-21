a20551fb16db97e0dc3752af012abd0688ed4a69
Warp Lord <fuddinsiddiqui@gmail.com>
2020-08-21 07:13:49 +0500

    ADd Social Media links in footer
    Add Custom Icon Font (created using Fontello)

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -5,6 +5,11 @@
 	<a href="/contact">Contact</a> |
 	<a href="/support">Support</a> |
 	<a href="/developers">Developers</a>
+	<div class="sm_links">
+		<a href="https://www.facebook.com/Sunnahcom-104172848076350" target="_blank"><i class="icn-fb"></i></a>
+		<a href="https://www.instagram.com/_sunnahcom/" target="_blank"><i class="icn-ig"></i></a>
+		<a href="https://twitter.com/SunnahCom" target="_blank"><i class="icn-twitter"></i></a>
+	</div>
 <div class=clear></div>
 </div>
 
@@ -14,6 +19,7 @@ var sc_project=7148282;
 var sc_invisible=1;
 var sc_security="63a57073";
 </script>
+<link href="<?php echo $this->context->auto_version('/css/fonts/fontello/css/ico-font.css'); ?>" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
 <noscript>
 <div class="statcounter"><a title="Web Analytics" href="https://statcounter.com/" target="_blank">
