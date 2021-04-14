963fe2ab3d6c52ec544241eea9f56890fad05124
naushervan <8232216+naushervan@users.noreply.github.com>
2021-04-14 21:08:11 +0500

    fixing report error, share, chapter intros

--- public/share.php
+++ public/share.php
@@ -1,18 +1,18 @@
 <?php
 ?>
 
-<div style="font-size: 16px; padding-top: 0px; padding-bottom: 16px; text-align: center;"> SHARE THIS HADITH</div>
-
-<input class=permalink_box type="text" value="https://sunnah.com<?php echo $_GET['link']; ?>" size=36 /><br>
-
+<h2>Share this Hadith</h2>
 <div class=share_buttons>
-	<div class="share_button" style="float: right; padding-top: 13px;">
+	<div class="share_button">
 		<a href="https://www.facebook.com/sharer.php?u=https://sunnah.com<?php echo $_GET['link']; ?>" target="blank" rel="noopener noreferrer" title="Share Hadith on Facebook" class="icn-fb"></a>
 	</div>
 
-	<div class="share_button" style="float: right; padding-top: 13px;">
+	<div class="share_button">
 		<a href="https://twitter.com/intent/tweet?url=https://sunnah.com<?php echo $_GET['link']; ?>&text=Hadith via @SunnahCom&hashtags=SunnahCom,hadith" target="blank" rel="noopener noreferrer" title="Share Hadith on Twitter" class="icn-twitter"></a>
 	</div>
 	<div class=clear></div>
 </div>
+
+<input class=permalink_box type="text" value="https://sunnah.com<?php echo $_GET['link']; ?>" size=36 /><br>
+
 
