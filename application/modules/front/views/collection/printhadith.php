878507e412449d5e5841f9f575ec9565e0f2ac58
ahadith <sunnah@iman.net>
2022-12-22 09:58:45 -0800

    Removing the left container for Arabic-only books

--- application/modules/front/views/collection/printhadith.php
+++ application/modules/front/views/collection/printhadith.php
@@ -31,20 +31,22 @@
 				echo "</div>";
             }
             echo "<div class=\"hadithTextContainers\" id=htc".$arabicURN.">";
-			echo "<div class=\"englishcontainer\" id=t".$arabicURN.">";
-			echo "<div class=\"english_hadith_full\">";
+			if ($book->status != 6) {
+				echo "<div class=\"englishcontainer\" id=t".$arabicURN.">";
+				echo "<div class=\"english_hadith_full\">";
 
-            $colon_match = preg_match("/[^0-9]:[^0-9]/", $englishText, $match, PREG_OFFSET_CAPTURE);
-            if (($colon_match === 1) and ($collection->name !== "hisn")) {
-                $narrated_part = substr($englishText, 0, $match[0][1] + 1);
-                $text_part = trim(substr($englishText, $match[0][1] + 2));
-                echo "<div class=hadith_narrated>".$narrated_part.":</div>";
-                echo "<div class=text_details>".$text_part."</div>\n";
-            }
-            else {
-                echo "<div class=text_details>".$englishText."</div>\n";
-            }
-            echo "<div class=clear></div></div></div>";
+				$colon_match = preg_match("/[^0-9]:[^0-9]/", $englishText, $match, PREG_OFFSET_CAPTURE);
+				if (($colon_match === 1) and ($collection->name !== "hisn")) {
+					$narrated_part = substr($englishText, 0, $match[0][1] + 1);
+					$text_part = trim(substr($englishText, $match[0][1] + 2));
+					echo "<div class=hadith_narrated>".$narrated_part.":</div>";
+					echo "<div class=text_details>".$text_part."</div>\n";
+				}
+				else {
+					echo "<div class=text_details>".$englishText."</div>\n";
+				}
+				echo "<div class=clear></div></div></div>";
+			}
 
             $arabicSanad1 = "";
             $arabicSanad2 = "";
