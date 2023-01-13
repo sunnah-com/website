a54127728d6374445602fdc6b58dcb2aac0a1fcd
ahadith <sunnah@iman.net>
2023-01-12 17:50:57 -0800

    Adding no auto-translate to <html> tag

--- application/modules/front/views/layouts/main.php
+++ application/modules/front/views/layouts/main.php
@@ -1,6 +1,6 @@
 <?php $this->beginPage() ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" translate="no">
 <head>
   <?php if (isset($this->params['book']) and (
 			//$this->params['book']->indonesianBookID > 0 or
